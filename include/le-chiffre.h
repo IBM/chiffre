1756e94917ef61d8439693ed8a5d0e44d0e7d493
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-01-02 15:33:52 -0500

    Add basic cycle test

--- include/le-chiffre.h
+++ include/le-chiffre.h
@@ -3,16 +3,26 @@
 #ifndef LE_CHIFFRE_TESTS_INCLUDE_LE_CHIFFRE_H_
 #define LE_CHIFFRE_TESTS_INCLUDE_LE_CHIFFRE_H_
 
-#define ADDR_SM 0x00000000
-#define ADDR_IM 0x60000000
-#define ADDR_DM 0x80000000
+#define f_ECHO 0
+#define f_CYCLE 1
+#define f_CHECK 2
 
 #ifndef CUSTOM_X
 #define CUSTOM_X 0
 #endif
 
-#define LE_CHIFFRE_ECHO(data, rd)                       \
-  li x1, data;                                          \
-  ROCC_INSTRUCTION_RAW_R_R_R(CUSTOM_X, rd, 1, 0, 0);
+#define LE_CHIFFRE_ECHO(data, rd)                               \
+  li x1, data;                                                  \
+  ROCC_INSTRUCTION_RAW_R_R_R(CUSTOM_X, rd, 1, 0, f_ECHO);
+
+// Send COUNT ones and SIZE-COUNT zeros through the scan chain.
+#define LE_CHIFFRE_CYCLE(size, count, rd)                       \
+  li x1, size;                                                  \
+  li x2, count;                                                 \
+  ROCC_INSTRUCTION_RAW_R_R_R(CUSTOM_X, rd, 1, 2, f_CYCLE);
+
+#define LE_CHIFFRE_CHECK(checksum, rd)                          \
+  li x1, checksum;                                              \
+  ROCC_INSTRUCTION_RAW_R_R_R(CUSTOM_X, rd, 1, 0, f_CHECK);
 
 #endif  // LE_CHIFFRE_TESTS_INCLUDE_LE_CHIFFRE_H_
