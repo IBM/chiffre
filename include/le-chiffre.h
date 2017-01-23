e12f80ce82a541a35b0db94707f3f8c136d98a21
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-01-23 13:56:31 -0500

    Add scan enable function, use it in cycle.S

--- include/le-chiffre.h
+++ include/le-chiffre.h
@@ -5,7 +5,7 @@
 
 #define f_ECHO 0
 #define f_CYCLE 1
-#define f_CHECK 2
+#define f_ENABLE 2
 
 #ifndef CUSTOM_X
 #define CUSTOM_X 0
@@ -20,8 +20,7 @@
   la x1, LABEL;                                                 \
   ROCC_INSTRUCTION_RAW_R_R_R(CUSTOM_X, rd, 1, 0, f_CYCLE);
 
-#define LE_CHIFFRE_CHECK(checksum, rd)                          \
-  li x1, checksum;                                              \
-  ROCC_INSTRUCTION_RAW_R_R_R(CUSTOM_X, rd, 1, 0, f_CHECK);
+#define LE_CHIFFRE_ENABLE(rd)                                   \
+  ROCC_INSTRUCTION_RAW_R_R_R(CUSTOM_X, rd, 0, 0, f_ENABLE);
 
 #endif  // LE_CHIFFRE_TESTS_INCLUDE_LE_CHIFFRE_H_
