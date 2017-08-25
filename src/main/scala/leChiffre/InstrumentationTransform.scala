a6f238bc9e778e0b7839226cd2c2db77a15f15a5
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-08-25 18:00:26 -0400

    [wip] Traversal working, algorithm sketch in place
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/leChiffre/InstrumentationTransform.scala
+++ src/main/scala/leChiffre/InstrumentationTransform.scala
@@ -7,6 +7,8 @@ import firrtl.ir._
 import firrtl.annotations._
 import firrtl.Mappers._
 
+import scala.collection._
+
 case class ModuleInstance(name: String, module: String)
 
 case class ModulePath(name: String, signal: ChiffreSignal, path: Seq[ModuleInstance])
@@ -42,148 +44,6 @@ class ChiffreInjectionTransform extends firrtl.Transform {
     }
   }
 
-  private def unimplemented(): Unit = { throw AnnotationException("unimplemented") }
-
-  private def findPath(moduleMap: Map[String, DefModule],
-    root: String, leaf: String): Seq[ModuleInstance] = {
-    val path = collection.mutable.ArrayBuffer[ModuleInstance]()
-
-    def dfs(c: ModuleInstance, leaf: String,
-      m: Map[String, DefModule],
-      p: collection.mutable.ArrayBuffer[ModuleInstance],
-      indent: String): Unit = {
-
-      def f(
-        m: Map[String, DefModule],
-        p: collection.mutable.ArrayBuffer[ModuleInstance],
-        l: String,
-        indent: String)(s: Statement): Statement = {
-
-        s map f(m, p, l, indent) match {
-          case WDefInstance(info, name, module, tpe) => {
-            if (p.size == 0) { dfs(ModuleInstance(name, module), l, m, p, indent) } }
-          case _ => }
-        s
-      }
-
-      if (c.module == leaf) { p += c }
-      else {
-        m(c.module) map f(m, p, leaf, indent + "  ")
-        if (p.size > 0) { p += c } }
-    }
-
-    dfs(ModuleInstance(root, root), leaf, moduleMap, path, "")
-
-    if (path.size == 0) {
-      throw AnnotationException(s"Found empty path from '${root}' -> '${leaf}' (likely due to deduplication)\n")
-    }
-
-    path.reverse
-  }
-
-  private def drill(signal: ChiffreSignal, source: Seq[ModuleInstance],
-    map: Map[String, DefModule], visited: Map[String, Port]):
-      (Map[String, DefModule], Option[Port], Map[String, Port]) = {
-    println(s"[info]     - drill '${source.head}' for signal '${signal}'")
-
-    val thisModule = map(source.head.module)
-    lazy val n = Namespace(thisModule)
-
-    val dir = if (signal.dir == "source") Output else Input
-    val (mapx, stmtx, portx, newModule, visitedx) = if (source.tail.nonEmpty) {
-      val (m, subPort, v) = drill(signal, source.tail, map, visited)
-      val p = Port(info,
-        n.newName(signal.name),
-        dir,
-        UIntType(IntWidth(signal.width.get)))
-      if (visited.contains(thisModule.name)) {
-        val s = if (dir == Output) {
-          Connect(info,
-            WRef(p.name, p.tpe, ExpKind, UNKNOWNGENDER),
-            WSubField(
-              WRef(source.tail(0).name, UnknownType, InstanceKind, UNKNOWNGENDER),
-              subPort.get.name, p.tpe, UNKNOWNGENDER))
-        } else {
-          Connect(info,
-            WSubField(
-              WRef(source.tail(0).name, UnknownType, InstanceKind, UNKNOWNGENDER),
-              subPort.get.name, p.tpe, UNKNOWNGENDER),
-            WRef(p.name, p.tpe, ExpKind, UNKNOWNGENDER))
-        }
-        val nm = thisModule match {
-          case m: Module => m copy (
-            info = m.info ++ info,
-            ports = m.ports :+ p,
-            body = Block(m.body +: Seq(s))) }
-        (m, s, Some(p), nm, v)
-      } else {
-        if (!subPort.isEmpty) {
-          val s = Connect(info,
-            WSubField(
-              WRef(source.tail(0).name, UnknownType, InstanceKind, UNKNOWNGENDER),
-              subPort.get.name, p.tpe, UNKNOWNGENDER),
-            WRef(p.name, p.tpe, ExpKind, UNKNOWNGENDER))
-          (m, s, Some(p), thisModule, v)
-        } else {
-          (m, EmptyStmt, None, thisModule, v)
-        }
-      }
-    } else if (source.tail.isEmpty && dir == Input) {
-      val p = Port(info,
-        n.newName(signal.name),
-        dir,
-        UIntType(IntWidth(signal.width.get)))
-      val nm = thisModule match {
-        case m: Module => m copy (
-          info = m.info ++ info,
-          ports = m.ports :+ p) }
-      (map, EmptyStmt, Some(p), nm, Map())
-    } else {
-      val p = thisModule.ports.filter(_.name == signal.name)
-      if (p.isEmpty) {
-        throw AnnotationException(s"Broadcast Source Module ${thisModule.name} does not contain port ${signal.name}") }
-      (map, EmptyStmt, Some(p.head), thisModule, Map())
-    }
-
-    (mapx ++ Map(newModule.name -> newModule), portx,
-      visited ++ Map(thisModule.name -> portx) ++ visitedx)
-  }
-
-  private def broadcastConnect(
-    c: Circuit,
-    source: ModulePath,
-    sink: ModulePath,
-    visited: Map[String, Port] = Map()):
-      (Circuit, Map[String, Port]) = {
-    val map = (c.modules map (m => m.name -> m)).toMap
-
-    println(s"[info] broadcastConnect '${source.signal.name}': ${source.name} -> ${sink.name}")
-
-    val (sourcePath, sinkPath) = {
-      val lcaIdx = (source.path
-        .zip(sink.path)
-        .map { case (l, r) => l != r }
-        .indexOf(true)) - 1
-      (source.path.drop(lcaIdx), sink.path.drop(lcaIdx)) }
-    println(s"[info]   - LCA:        ${sourcePath(0)}")
-    println(s"[info]   - sourcePath: ${sourcePath}")
-    println(s"[info]   - sinkPath:   ${sinkPath}")
-    println(s"[info]   - visited (pre drill):   ${visited}")
-
-    // Drill out the source path
-    val (mx, _, vx) = drill(source.signal, sourcePath.tail, map, visited)
-
-    // Drill out the sink path
-    val (mxx, _, vxx) = drill(sink.signal, sinkPath.tail, mx, vx)
-
-    // Connect the source path to the sink path
-    // [todo]
-
-    println(s"[info]   - visited (post drill):   ${vx}")
-
-    (c copy(modules = (mxx values).toSeq), vxx)
-  }
-
   private def broadcastInstrument(
     c: Circuit,
     port: Port,
@@ -192,34 +52,57 @@ class ChiffreInjectionTransform extends firrtl.Transform {
       Circuit = {
 
     object Color extends Enumeration { val Grey, White, Red, Green = Value }
-    case class Data(module: DefModule, color: Color)
+    case class Data(var module: DefModule, var color: Color.Value,
+      val subColors: mutable.ArrayBuffer[Color.Value] = mutable.ArrayBuffer())
     case class GreenPorts(module: DefModule, port: Port)
-    val state = collection.mutable.Map[String, Data](
+    val state = mutable.Map[String, Data](
       c.modules.map((m) => m.name -> Data(m, Color.Grey)): _*)
-    val queue = collection.mutalbe.MutableList[GreenPorts]()
+    val queue = mutable.MutableList[GreenPorts]()
+
+    println(s"""[info] broadcastInstrument
+[info]   - circuit: ${c.main}
+[info]   - port:    ${port.name}
+[info]   - source:  ${source}
+[info]   - sinks:   ${sinks}""")
 
     def flip(p: Port): Port = {
-      p copy (
-        direction = p.direction match {
-          case i: Input  => Output
-          case o: Output => Input })
+      p
+      // p copy (
+      //   direction = p.direction match {
+      //     case i: firrtl.ir.Input  => Output
+      //     case o: firrtl.ir.Output => Input })
     }
 
-    def addPorts(m: DefModule, p: Port): DefModule = {
+    def addPort(m: DefModule, p: Port): DefModule = {
       if (m.ports contains p) { return m }
-      m copy (
+      m match { case m: Module => m copy (
         info = m.info ++ info,
-        ports = m.ports :+ p)
+        ports = m.ports :+ p) }
     }
 
-    def connect() = {
-    }
+    def traverse(modName: String, parentName: Option[String] = None): Unit = {
+      // [todo]
+      //   * Handle sink below sink
+      //   * Handle sink below source
 
-    def traverse(modName: String): Color = {
-      println(s"[info] Traversing: ${modname}")
+      def dfs(modName: String)(s: Statement): Statement = {
+        s map dfs(modName) match {
+          case WDefInstance(_, _, module, _) =>
+            traverse(modName = module, parentName = Some(modName))
+          case _ =>
+        }
+        s
+      }
+
+      println(s"[info] Traversing: ${modName}")
       // Exit if we've already visited this module. Otherwise,
       // preemptively set the color to white (unvisited).
-      if (state(modName).color != Color.Grey) { return state(modName).color }
+      if (state(modName).color != Color.Grey) {
+        if (parentName.nonEmpty) {
+          state(parentName.get).subColors += state(modName).color }
+        println(s"[info]   - exiting because 'already visited'")
+        return
+      }
 
       // This is a source:
       //   * Add a port if they do not exist
@@ -227,108 +110,84 @@ class ChiffreInjectionTransform extends firrtl.Transform {
       //   * Return as no sinks can exist beneath a source (currently)
       if (source == modName) {
         state(modName).color = Color.Green
-        queue += GreenPorts(state(modName), port)
-        return color.Green }
+        queue += GreenPorts(state(modName).module, port)
+        if (parentName.nonEmpty) {
+          state(parentName.get).subColors += Color.Green }
+        println(s"[info]   - exiting because 'is source'")
+        return
+      }
 
       // This is a sink:
       //   * Add a port if it does not exist
       //   * Color this module RED (on the sink path)
       if (sinks contains modName) {
-        state(modName) = (addPort(state(modName).module, flip(port)), Color.Red) }
+        state(modName) = Data(addPort(state(modName).module, flip(port)), Color.Red)
+        if (parentName.nonEmpty) {
+          state(parentName.get).subColors += Color.Red }
+        println(s"[info]   - exiting because 'is sink'")
+        return
+      }
 
-      // Recurse to compute the colors of all instances then take
-      // action based on the instance colors
-      //   * Green and not Red: This may be the least common ancestor
-      //     (LCA). However, we can't be sure until the full DFS
-      //     completes. This module is pushed onto the queue.
-      //   * Green (and Red): Connect all reds and push this onto the
-      //     queue.
-      //   * Red: Connect all reds and add ports going upwards
-      val colors = state(modName).module map ((s: Statement) => s match {
-        case i: DefInstance => i.map(traverse(_.module)) })
-      state(modName).color = colors map (c =>
-        if (c contains Color.Green & c contains Color.Red) {
+      // Recurse to compute the colors of all submodules
+      state(modName).module map dfs(modName)
+
+      // Determine the coloring of this module base on submodule colors
+      state(modName).color = {
+        val subColors = state(modName).subColors
+        if ((subColors contains Color.Green) & (subColors contains Color.Red)) {
           // Clear queue by connecting everything in the queue
           queue.clear
           queue += GreenPorts(state(modName).module, port)
           Color.Green
         }
-        else if (c contains Color.Green & !c contains Color.Red) {
-          queue += GreenPorts(state(modName), port)
+        else if ((subColors contains Color.Green) & !(subColors contains Color.Red)) {
+          queue += GreenPorts(state(modName).module, port)
           Color.Green
         }
-        else if (c contains Color.Red) {
-          addPorts(state(modName).module, port)
+        else if (subColors contains Color.Red) {
+          addPort(state(modName).module, port)
           Color.Red
         }
         else {
           Color.White
-        })
-
-      state(modName).color
+        }}
+      if (parentName.nonEmpty) {
+        state(parentName.get).subColors += Color.Green }
     }
 
-    traverse(c.main.name)
+    traverse(c.main)
+
+    // [todo] copy over all the modules into the new circuit
+    c
   }
 
   private def run(c: Circuit, modules: Seq[(String, ChiffreSignal)]): Circuit = {
     print(s"[info] in 'run' with ${modules}\n")
-
-    // val map = (c.modules map (m => m.name -> m)).toMap
-
-    // val paths = modules
-    //   .map { case (name, signal) =>
-    //     ModulePath(name, signal, findPath(map, c.main, name)) }
-
-    // val broadcastSources = paths.filter {
-    //   case ModulePath(n, s, p) => s.tpe == "broadcast" && s.dir == "source" }
-    // val broadcastSinks = paths.filter {
-    //   case ModulePath(n, s, p) => s.tpe == "broadcast" && s.dir == "sink" }
-
-    // println(s"[info] Broadcast Sources:")
-    // for (x <- broadcastSources) { println(s"[info]   - ${x}") }
-    // println(s"[info] Broadcast Sinks:")
-    // for (x <- broadcastSinks)   { println(s"[info]   - ${x}") }
-
-    // broadcastSources
-    //   .foldLeft(c) {
-    //     case (circuit, sourcePath) => {
-    //       val ModulePath(_, ChiffreSignal(_, _, sourceName, width), _) = sourcePath
-    //       val sinks = broadcastSinks.filter {
-    //         case ModulePath(_, ChiffreSignal(_, _, n, _), _) => n == sourceName }
-    //       require (sinks.size != 0, s"Found zero sinks for source ${sourceName}")
-    //       sinks.map(_.signal.width = width)
-    //       val (c, _) = sinks.foldLeft(circuit, Map[String, Port]()){
-    //         case ((c, v), sinkPath) => broadcastConnect(c, sourcePath, sinkPath, v) }
-    //       c
-    //     }}
 
     case class ChiffreSignal(tpe: String, dir: String, name: String,
       var width: Option[Int] = None)
 
     val broadcastSources: Seq[(Port, String)] = modules
       .filter{ case (_, s) => s.tpe == "broadcast" && s.dir == "source" }
-      .map{ case(modName, ChiffreSignal(_, _, portName, portWidth)) =>
+      .map{ case(modName, port) =>
         (Port(
           info = info,
-          name = portName,
+          name = port.name,
           direction = Output,
-          tpe = UIntType(IntWidth(portWidth.get))
+          tpe = UIntType(IntWidth(port.width.get))
         ),
           modName) }
     println(s"[info] broadcastSources: ${broadcastSources}")
 
     val broadcastSinks: Map[String, Seq[String]] = modules
       .filter{ case (_, s) => s.tpe == "broadcast" && s.dir == "sink" }
-      .map{ case(modName, ChiffreSignal(_, _, portName, portWidth)) =>
-        (portName, modName) }
+      .map{ case(modName, port) => (port.name, modName) }
       .groupBy(_._1)
       .map{ case(k, v) => (k, v.map(_._2)) }
     println(s"[info] broadcastSinks: ${broadcastSinks}")
 
     broadcastSources
-      .foldLeft(c)
-      .map{ case (port, modName) =>
-        broadcastInstrument(c, port, modName, broadcastSinks(port.name)) }
+      .foldLeft(c){ case (circuit, (port, modName)) =>
+        broadcastInstrument(circuit, port, modName, broadcastSinks(port.name)) }
   }
 }
