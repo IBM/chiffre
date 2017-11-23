81eb8194373cd82f6dbd0ccc60ac49b176c29c8b
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-11-22 23:03:45 -0500

    Add StuckAt injector
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

---
+++ src/main/scala/leChiffre/inject/StuckAt.scala
@@ -0,0 +1,27 @@
+// See LICENSE.IBM for license details
+package leChiffre.inject
+
+import chisel3._
+import chisel3.util._
+import leChiffre.scan._
+
+class StuckAt(n: Int, id: String) extends Injector(n, id) {
+  val mask = Reg(UInt(n.W))
+  val value = Reg(UInt(n.W))
+  val enabled = Reg(init = false.B)
+
+  lazy val bits = Seq( Mask(n) )
+
+  io.out := enabled & (
+    (!mask & io.in) | ((mask & io.in) & value) | ((mask & !io.in) | value) )
+
+  when (io.scan.clk) {
+    enabled := false.B
+    mask := io.scan.in ## (mask >> 1)
+    value := mask(0) ## (value >> 1)
+  }
+
+  io.scan.out := value(0)
+
+  when (io.scan.en) { enabled := true.B }
+}
