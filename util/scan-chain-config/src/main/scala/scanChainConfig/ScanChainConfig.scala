d725fef6bf75b9a816d831ec156e775f22f13670
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-12-12 15:18:52 -0500

    [wip] Untested binary configuration emission
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- util/scan-chain-config/src/main/scala/scanChainConfig/ScanChainConfig.scala
+++ util/scan-chain-config/src/main/scala/scanChainConfig/ScanChainConfig.scala
@@ -3,6 +3,7 @@ package scanChainConfig
 
 import scopt._
 import java.io.File
+import java.io.FileOutputStream
 import scala.io.Source
 import leChiffre.scan._
 
@@ -13,6 +14,7 @@ case class ScanChainException(msg: String) extends Exception(msg)
 
 case class Arguments(
   scanChainFileName: File = new File("."),
+  scanChainDir: Option[File] = None,
   verbose: Boolean = false,
   probability: Option[Double] = None,
   seed: Option[Int] = None
@@ -29,8 +31,10 @@ class ScanChainUtils(implicit opt: Arguments) {
   def getComponentNames(s: Seq[FaultyComponent]): Seq[String] = s
     .map(_.name)
 
-  def bind(f: ScanField, name: String)(implicit opt: Arguments): ScanField =
+  private def bind(f: ScanField, name: String)(implicit opt: Arguments): ScanField =
     f match {
+      case x: ScanField if x.value.nonEmpty => throw new Exception(
+        s"Tried to rebind already bound ScanField $f (name: $name)")
       case x: Seed => x.copy(value = Some(BigInt(x.width, rand)))
       case x: Difficulty => x
           .copy(probability =
@@ -41,30 +45,61 @@ class ScanChainUtils(implicit opt: Arguments) {
       case _ => throw new Exception(s"Unimplemented binding for ScanField $f")
     }
 
-  def bind(i: InjectorInfo, name: String)(implicit opt: Arguments): InjectorInfo = {
+  private def bind(i: InjectorInfo, name: String)(implicit opt: Arguments): Unit =
     i.fields = i.fields.map(bind(_, name))
-    i
+
+  private def bind(f: FaultyComponent)(implicit opt: Arguments): Unit =
+    bind(f.injector, f.name)
+
+  /* Consume command line options to populate scan chain fields */
+  def bind(s: ScanChain)(implicit opt: Arguments): Unit = s.foreach {
+    case (name, chain) => { chain.foreach { bind(_) } }
   }
 
-  def bind(f: FaultyComponent)(implicit opt: Arguments): FaultyComponent = {
-    f.copy(injector = bind(f.injector, f.name))
-  }
+  /* Convert a Scan Chain description to a bit string */
+  def bitString(c: Seq[FaultyComponent]): String = c.map(_.toBits()).mkString
 
-  def bind(s: ScanChain)(implicit opt: Arguments): ScanChain = {
-    s.map {
-      case (name, chain) => { chain.map { bind(_) } }
-        (name, chain)
-    }
-  }
-
-  def toString(chain: ScanChain, indent: String = ""): String = {
-    chain
-      .map{case(k, v) =>
-        s"""|${indent}${k}:
-            |${v.map(_.serialize(indent + "  ")).mkString("\n")}
-            |${indent}  raw: ${v.map(_.toBits()).mkString}"""
+  /* Pretty-print all scan chains */
+  def serialize(chains: ScanChain, indent: String = ""): String = {
+    chains
+      .map{case(name, c) =>
+        s"""|${indent}${name}:
+            |${indent}  length: ${getLength(c)}b
+            |${indent}  chain:
+            |${c.map(_.serialize(indent + "    ")).mkString("\n")}
+            |${indent}  raw: ${bitString(c)}"""
           .stripMargin}
       .mkString("\n")
+  }
+
+  def fletcher(x: String, n: Int = 32): BigInt = {
+    var a: BigInt = 0
+    var b: BigInt = 0
+    x.grouped(16).toList.reverse.foreach { xx =>
+      val word = BigInt(xx, 2)
+      a = (a + word) % (1 << n/2)
+      b = (a + b) % (1 << n/2)
+    }
+    (b << n/2) + a
+  }
+
+  def padding(x: BigInt, width: Int): String = s"%${width}s"
+    .format(x.toString(2))
+    .replace(' ', '0')
+
+  def toBinary(chain: Seq[FaultyComponent]): Array[Byte] = {
+    val length = getLength(chain)
+    val pad = Seq.fill(((-length - 31) % 32) + 31)('0').mkString
+    val bits = (pad ++ bitString(chain)).mkString
+    println(s"  [info]   pad: $bits")
+    val checksum = fletcher(bits, 32)
+    println(s"  [info] checksum: $checksum (0b${padding(checksum, 32)})")
+    println(s"  [info] length: $length (0b${padding(length, 32)})")
+    val raw = bits ++ padding(checksum, 32) ++ padding(length, 32)
+    println(s"""  [info] raw: ${raw.grouped(8).mkString(" ")}""")
+    println(s"""  [info] raw: ${raw.grouped(4).toList.map(BigInt(_, 2).toString(16)).mkString.grouped(16).mkString(" ")}""")
+    println(s"  [info] bytes: ${raw.grouped(8).toArray.map(BigInt(_, 2).toByte).mkString(" ")}")
+    raw.grouped(8).toArray.reverse.map(BigInt(_, 2).toByte)
   }
 }
 
@@ -75,6 +110,9 @@ object Main extends App {
     opt[Unit]("verbose")
       .action( (_, c) => c.copy(verbose = true) )
       .text("Enable verbose output")
+    opt[File]('o', "output-dir")
+      .action( (x, c) => c.copy(scanChainDir = Some(x)) )
+      .text("")
     opt[Double]('p', "probability")
       .action( (x, c) => c.copy(probability = Some(x)) )
       .validate( x => if (x >= 0 && x <= 1) success
@@ -84,7 +122,7 @@ object Main extends App {
       .action( (x, c) => c.copy(seed = Some(x)) )
       .validate( x => if (x >= 0) success
                 else failure("the seed <value> must be greater than or equal to 0") )
-      .text("Randome number seed")
+      .text("Random number seed")
     arg[File]("scan.yaml")
       .required()
       .action( (x, c) => c.copy(scanChainFileName = x) )
@@ -100,18 +138,25 @@ object Main extends App {
         .parseYaml
         .convertTo[ScanChain]
 
-      if (opt.verbose) {
-        chains.foreach{ case (name, c) =>
-          println(
-            s"  [info] Found scan chain '$name' of length ${util.getLength(c)}b")
-          println(
-            util.getComponentNames(c).foreach{ n =>
-              println(s"  [info]   - $n") })
+      util.bind(chains)
+
+      if (opt.scanChainDir.nonEmpty) {
+        val dir = opt.scanChainDir.get
+        if (!dir.exists) dir.mkdir()
+        chains.foreach { case (name, c) =>
+          val w = new FileOutputStream(new File(dir, s"${name}.bin"))
+          val bytes = util.toBinary(c)
+          println(bytes)
+          bytes.foreach(w.write(_))
+          w.close()
         }
       }
 
-      util.bind(chains)
-      println(util.toString(chains))
+      if (opt.verbose)
+        println(util.serialize(chains, "  [info] "))
+
+      chains.foreach{ case (name, c) => util.toBinary(c) }
+
     case None =>
   }
 }
