77643250a4ad3d859cdbf4a2a3a57ab2f84194f6
Alec Roelke <alec.roelke@baesystems.com>
2018-07-05 08:55:04 -0400

    Change isFaulty to support any kind of Data
    Signed-off-by: Alec Roelke <alec.roelke@baesystems.com>

--- src/main/scala/chiffre/Instrumentation.scala
+++ src/main/scala/chiffre/Instrumentation.scala
@@ -61,8 +61,8 @@ trait ChiffreInjectee extends BaseModule {
 
   def isFaulty[T <: Injector](component: InstanceId, id: String, gen: Class[_ <: Injector]): Unit = {
     component match {
-      case c: Bits =>
-        chisel3.experimental.annotate(
+      case c: Data =>
+        annotate(
           new ChiselAnnotation with RunFirrtlTransform {
             def toFirrtl = FaultInjectionAnnotation(c.toNamed, id, gen)
             def transformClass = classOf[FaultInstrumentationTransform]
