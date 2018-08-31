d8703986633f38d0ecf25d6c3b5614a0772c61fb
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2018-08-31 14:04:20 -0400

    Minor cleanup to Instrumentation.scala
    - Add ScalaDoc for isFaulty
    - Remove commented-out code
    - Make ChiffreController use a self-type instead of inheritance
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/chiffre/Instrumentation.scala
+++ src/main/scala/chiffre/Instrumentation.scala
@@ -21,17 +21,12 @@ import chiffre.passes.{ScanChainAnnotation, FaultInjectionAnnotation,
 import chiffre.inject.Injector
 import chiffre.passes.ScanChainDescriptionAnnotation
 
-trait ChiffreController extends BaseModule {
-  self: BaseModule =>
-
+trait ChiffreController { this: BaseModule =>
   /** Scan Chain Identifier used to differentiate scan chains. This must
     * be a `lazy val`. */
   def scanId: String
 
   private def scanMaster(scan: Data, name: String): Unit = {
-    // if (scanId == null) { // scalastyle:off
-    //   throw new Exception(
-    //     "Chiffre Controller 'scanId' should be a 'lazy val'") }
     chisel3.experimental.annotate(
       new ChiselAnnotation {
         def toFirrtl = ScanChainAnnotation(scan.toNamed, "master", "scan", name, None)
@@ -50,6 +45,13 @@ trait ChiffreInjector { this: Injector =>
 }
 
 trait ChiffreInjectee { this: BaseModule =>
+
+  /** Make a specific signal run-time fault injectable
+    *
+    * @param component the component to make injectable
+    * @param id the name of the scan chain used to configure injections into this component
+    * @param gen a class of [[Injector]] to use for fault injections
+    */
   def isFaulty(component: Data, id: String, gen: Class[_ <: Injector]): Unit = {
     chisel3.experimental.annotate(
       new ChiselAnnotation with RunFirrtlTransform {
